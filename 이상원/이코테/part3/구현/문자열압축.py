0f51971ee35f3872f05e1666794768d03f481064
s-wlii <marshmello.sw@gmail.com>
2022-02-06 14:19:08 +0900

    amend files

--- 이상원/이코테/part3/구현/문자열압축.py
+++ 이상원/이코테/part3/구현/문자열압축.py
@@ -32,7 +32,31 @@ def solution(s):
     return answer
 
 
+def booksolution(s):
+    answer = len(s)
+    # 1개 단위(step)부터 압축 단위를 늘려가며 확인
+    for step in range(1, len(s) // 2 + 1):
+        compressed = ""
+        prev = s[0:step]
+        count = 1
+        # 단위 크기만큼 증가시키며 이전 문자열과 비교
+        for j in range(step, len(s), step):
+            # 이전 상태와 동일하다면 압축 횟수 증가
+            if prev == s[j : j + step]:
+                count += 1
+            # 다른 문자열이 나오면
+            else:
+                compressed += str(count) + prev if count >= 2 else prev
+                prev = s[j : j + step]  # 다시 상태 초기화
+                count = 1
+        # 남아 있는 문자열에 대해 처리
+        compressed += str(count) + prev if count >= 2 else prev
+        # 만들어지는 압축 문자열이 가장 짧은 것이 정답
+        answer = min(answer, len(compressed))
+    return answer
+
+
 s = ["aabbaccc", "ababcdcdababcdcd", "abcabcdede", "abcabcabcabcdededededede", "xababcdcdababcdcd"]
 
 for i in s:
-    print(solution(i))
+    print(booksolution(i))
