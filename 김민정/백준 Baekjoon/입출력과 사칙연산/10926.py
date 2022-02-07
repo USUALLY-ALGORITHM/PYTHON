82c5ceaf46abeb790f1165b336541c7df163de50
김민정 <94227598+isakacindy@users.noreply.github.com>
2022-02-07 23:51:06 +0900

    Add files via upload

---
+++ 김민정/백준 Baekjoon/입출력과 사칙연산/10926.py
@@ -0,0 +1,4 @@
+# ??! (https://www.acmicpc.net/problem/10926)
+
+id = input()
+print(id + '??!')
---
+++ 김민정/백준 Baekjoon/입출력과 사칙연산/18108.py
@@ -0,0 +1,4 @@
+# 1998년생인 내가 태국에서는 2541년생?! (https://www.acmicpc.net/problem/18108)
+
+y = int(input())
+print(y - 543)
