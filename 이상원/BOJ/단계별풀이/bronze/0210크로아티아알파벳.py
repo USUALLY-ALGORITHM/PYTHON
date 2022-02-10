2a2b4c0cf9d5af1778569994f284534691597d61
s-wlii <marshmello.sw@gmail.com>
2022-02-11 00:03:32 +0900

    add files

---
+++ 이상원/BOJ/단계별풀이/bronze/0210크로아티아알파벳.py
@@ -0,0 +1,17 @@
+"""
+https://www.acmicpc.net/problem/2941
+"""
+alpha = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z"]
+n = str(input())
+
+result = 0
+target = ""
+for i in n:
+    for j in alpha:
+        if i ==j[0]:
+            
+            
+        
+        
+
+print(result)
