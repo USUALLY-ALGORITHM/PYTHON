3aa469be26d9c66a85e8f5c35a2d4d8ee3b80cd3
김민정 <94227598+isakacindy@users.noreply.github.com>
2022-02-25 20:46:47 +0900

    Delete 김민정/백준 Baekjoon/while문 directory

--- 김민정/백준 Baekjoon/while문/10951.py
+++
@@ -1,8 +0,0 @@
-# A + B - 4 (https://www.acmicpc.net/problem/10951)
-
-while True:
-    try:
-        A, B = map(int, input().split())
-        print(A + B)
-    except:
-        break
--- 김민정/백준 Baekjoon/while문/10952.py
+++
@@ -1,7 +0,0 @@
-# A + B - 5 (https://www.acmicpc.net/problem/10952)
-
-while True:
-    A, B = map(int, input().split())
-    if A == 0 and B == 0:
-        break
-    print(A + B)
