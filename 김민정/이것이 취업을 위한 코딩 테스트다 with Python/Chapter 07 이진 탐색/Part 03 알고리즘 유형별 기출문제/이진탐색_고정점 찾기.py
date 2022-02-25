968601af3c0952604f9a9e8c87e0542719380a10
김민정 <94227598+isakacindy@users.noreply.github.com>
2022-02-25 22:04:35 +0900

    Add files via upload

---
+++ 김민정/이것이 취업을 위한 코딩 테스트다 with Python/Chapter 07 이진 탐색/Part 03 알고리즘 유형별 기출문제/이진탐색_고정점 찾기.py
@@ -0,0 +1,41 @@
+N = int(input())
+array = list(map(int, input().split()))
+
+flag = 0
+for i in range(N):
+    if i == array[i]:
+        flag = 1
+        print(i)
+        break
+if flag == 0:
+    print(-1)
+
+"""
+# 이진 탐색 소스코드 구현(재귀 함수)
+def binary_search(array, start, end):
+    if start > end:
+        return None
+    mid = (start + end) // 2
+    # 고정점을 찾은 경우 인덱스 반환
+    if array[mid] == mid:
+        return mid
+    # 중간점이 가리키는 위치의 값보다 중간점이 작은 경우 왼쪽 확인
+    elif array[mid] > mid:
+        return binary_search(array, start, mid - 1)
+    # 중간점이 가리키는 위치의 값보다 중간점이 큰 경우 오른쪽 확인
+    else:
+        return binary_search(array, mid + 1, end)
+        
+    n = int(input())
+    array = list(map(int, input().split()))
+    
+    # 이진 탐색 수행
+    index = binary_search(array, 0, n - 1)
+    
+    # 고정점이 없는 경우 -1 출력
+    if index == None:
+        print(-1)
+    # 고정점이 있는 경우 해당 인덱스 출력
+    else:
+        print(index)
+"""
