15bbc4b6820ce1ff2e71048ddbfa4f5031b9ed07
nowgnas <marshmello.sw@gmail.com>
2022-02-26 00:08:25 +0900

    solved prob

--- 이상원/BOJ/단계별풀이/silver/좌표정렬하기.py
+++ 이상원/BOJ/단계별풀이/silver/좌표정렬하기.py
@@ -1,9 +1,17 @@
-'''
+"""
 https://www.acmicpc.net/problem/11650
-'''
+"""
 
-n = int(input())
+import sys
 
+sysIn = sys.stdin.readline
+
+n = int(sysIn())
+arr = []
 for i in range(n):
-    
-    
+    x, y = map(int, sysIn().split())
+    arr.append((x, y))
+
+arr.sort(key=lambda x: (x[0], x[1]))
+for i, j in arr:
+    print(f"{i} {j}")
