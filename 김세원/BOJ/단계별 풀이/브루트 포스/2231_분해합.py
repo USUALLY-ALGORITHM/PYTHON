3e4c220ed557f23b2f5009e2678713b4d2c2cb6b
sewon00 <ksw6889@naver.com>
2022-03-08 16:18:55 +0900

    add file

---
+++ 김세원/BOJ/단계별 풀이/브루트 포스/2231_분해합.py
@@ -0,0 +1,13 @@
+n = int(input())
+result = 0
+
+for i in range(1, n+1):
+    data = list(map(int, str(i)))
+    result = i + sum(data)
+
+    if result == n:
+        print(i)
+        break
+
+    if i == n:
+        print(0)
