4dea5a4d928e5690d7f74cd243b8c7a036a5093a
nowgnas <marshmello.sw@gmail.com>
2022-03-15 23:13:46 +0900

    Update README add schedule

--- 이상원/BOJ/다이나믹프로그래밍/0314오르막수.py
+++ 이상원/BOJ/다이나믹프로그래밍/0314오르막수.py
@@ -1,12 +1,19 @@
 '''
 https://www.acmicpc.net/problem/11057
+
+점화식의 규칙을 찾는 것이 중요하다.
 '''
 
-import sys
+n = int(input())
 
+dp = list([[1] * 10])
 
-inputs = sys.stdin.readline
+for _ in range(n):
+    dp.append(list([0] * 10))
 
-dp = [0]*1001
+for i in range(1, n+1):
+    for j in range(0, 10):
+        for col in range(j+1):
+            dp[i][j] += dp[i-1][col]
 
-n = int(inputs())
+print(dp[n][9] % 10007)
--- 이상원/이코테/part2/chapter10그래프/크루스칼.py
+++ 이상원/이코테/part2/chapter10그래프/크루스칼.py
@@ -17,33 +17,34 @@ def union_parent(parent, a, b):
         parent[a] = b
 
 
-# 노드의 개수와 간선(Union 연산)의 개수 입력 받기
-v, e = map(int, input().split())
-parent = [0] * (v + 1)  # 부모 테이블 초기화하기
+if __name__ == '__main__':
+    # 노드의 개수와 간선(Union 연산)의 개수 입력 받기
+    v, e = map(int, input().split())
+    parent = [0] * (v + 1)  # 부모 테이블 초기화하기
 
-# 모든 간선을 담을 리스트와, 최종 비용을 담을 변수
-edges = []
-result = 0
+    # 모든 간선을 담을 리스트와, 최종 비용을 담을 변수
+    edges = []
+    result = 0
 
-# 부모 테이블상에서, 부모를 자기 자신으로 초기화
-for i in range(1, v + 1):
-    parent[i] = i
+    # 부모 테이블상에서, 부모를 자기 자신으로 초기화
+    for i in range(1, v + 1):
+        parent[i] = i
 
-# 모든 간선에 대한 정보를 입력 받기
-for _ in range(e):
-    a, b, cost = map(int, input().split())
-    # 비용순으로 정렬하기 위해서 튜플의 첫 번째 원소를 비용으로 설정
-    edges.append((cost, a, b))
+    # 모든 간선에 대한 정보를 입력 받기
+    for _ in range(e):
+        a, b, cost = map(int, input().split())
+        # 비용순으로 정렬하기 위해서 튜플의 첫 번째 원소를 비용으로 설정
+        edges.append((cost, a, b))
 
-# 간선을 비용순으로 정렬
-edges.sort()
+    # 간선을 비용순으로 정렬
+    edges.sort()
 
-# 간선을 하나씩 확인하며
-for edge in edges:
-    cost, a, b = edge
-    # 사이클이 발생하지 않는 경우에만 집합에 포함
-    if find_parent(parent, a) != find_parent(parent, b):
-        union_parent(parent, a, b)
-        result += cost
+    # 간선을 하나씩 확인하며
+    for edge in edges:
+        cost, a, b = edge
+        # 사이클이 발생하지 않는 경우에만 집합에 포함
+        if find_parent(parent, a) != find_parent(parent, b):
+            union_parent(parent, a, b)
+            result += cost
 
-print(result)
+    print(result)
