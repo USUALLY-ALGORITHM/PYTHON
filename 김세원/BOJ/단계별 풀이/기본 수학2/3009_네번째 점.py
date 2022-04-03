48b2d0c3ead815917a6c7a262b2ecf6778fec37d
nowesmik <76905272+nowesmik@users.noreply.github.com>
2022-04-03 23:52:30 +0900

    add file

---
+++ 김세원/BOJ/단계별 풀이/기본 수학2/3009_네번째 점.py
@@ -0,0 +1,21 @@
+x1,y1 = map(int,input().split())
+x2,y2 = map(int,input().split())
+x3,y3 = map(int,input().split())
+x4 = 0 
+y4 = 0
+
+if x1 == x2:
+    x4 = x3
+elif  x2 == x3: 
+    x4 = x1
+elif x1 == x3: 
+    x4 = x2
+
+if y1 == y2:
+    y4 = y3
+elif  y2 == y3: 
+    y4 = y1
+elif y1 == y3: 
+    y4 = y2
+
+print(x4,y4)
