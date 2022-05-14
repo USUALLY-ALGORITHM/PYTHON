9dc4b928877a46f9ff86acebe06d4cd4f624c72b
nowgnas <marshmello.sw@gmail.com>
2022-05-14 18:50:07 +0900

    add files

--- 이상원/programmers/0512좌물쇠와열쇠.py
+++ 이상원/programmers/0512좌물쇠와열쇠.py
@@ -68,12 +68,6 @@ def solution(key, lock):
     return False
 
 
-# def solution(key, lock):
-#     answer = True
-
-#     return answer
-
-
 if __name__ == '__main__':
     key = [[0, 0, 0], [1, 0, 0], [0, 1, 1]]
     lock = [[1, 1, 1], [1, 1, 0], [1, 0, 1]]
