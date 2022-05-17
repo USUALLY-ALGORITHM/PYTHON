f8a6c22a74ee4c240ad7602c7caab46d9e6b7e9b
isakacindy <94227598+isakacindy@users.noreply.github.com>
2022-05-17 21:15:09 +0900

    Add files via upload

---
+++ 김민정/삼성 SW Expert Academy/Lv1/2047(신문 헤드라인).py
@@ -0,0 +1,5 @@
+# 신문 헤드라인 (https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=1&contestProbId=AV5QKsLaAy0DFAUq&categoryId=AV5QKsLaAy0DFAUq&categoryType=CODE&problemTitle=&orderBy=FIRST_REG_DATETIME&selectCodeLang=ALL&select-1=1&pageSize=10&pageIndex=1&&&&&&&&&&)
+
+sentence = input()
+result = sentence.upper()
+print(result)
