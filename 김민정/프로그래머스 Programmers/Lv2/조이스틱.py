265feeaaca013d22c28b1fdf9a40d8610d5baa03
isakacindy <94227598+isakacindy@users.noreply.github.com>
2022-07-28 13:48:16 +0900

    Add files via upload

---
+++ 김민정/프로그래머스 Programmers/Lv2/조이스틱.py
@@ -0,0 +1,17 @@
+def solution(name):
+    answer = 0
+    min_move = len(name) -1
+    for i in range(len(name)):
+        answer += min(ord(name[i])-ord("A"),ord("Z")+1-ord(name[i]))
+
+        next = i + 1
+        while next < len(name) and name[next] == 'A':
+            next += 1
+
+        min_move = min([min_move, 2 *i + len(name) - next, i + 2 * (len(name) -next)])
+    answer += min_move
+
+    return answer
+
+
+# https://zoozoozoo.tistory.com/110
